af75df3bc71eec6d72333e20d9f7be3cc2019085
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-06-03 18:05:23 +0300

    .

--- strings/hamming.s
+++ strings/hamming.s
@@ -1,39 +1,31 @@
 .section .data
 
 EOS_mask:
-  .byte 0x1, 0xFF
-  .byte 0,0,0,0,0,0,0,0,0,0
-
+  .byte 0x1,0xFF
+  .byte 0,0,0,0,0,0,0,0,0,0,0,0
 
 .section .text
 
 .globl hamming_dist
 
-# hamming_dist:
-#   call tirgul_strlen
-# 	ret
-
-
-# tirgul_strlen:
-
 hamming_dist:
   push %rbp
   mov %rsp, %rbp
 
-  # Zero our counting registers
+  ;Zero our counting registers
   xor %rax, %rax
   xor %rcx, %rcx
 
-  # Load mask into xmm1
-  lea EOS_mask(%rip), %rsi
-  movq %rsi, %xmm1
+  ;Load mask into xmm1
+  lea EOS_mask, %rsi
+  movdqu (%rsi), %xmm1
 
 .loop:
-  movdqu (%rdi, %rax), %xmm0  # Load data from string into xmm0
-  pcmpistri $0x18, %xmm1, %xmm0  # Control byte in immediate, compare xmm1 with xmm0
+  add %rax, %rcx
+  pcmpistri $0b00010100, (%rdi,%rax), %xmm1
   jnz .loop
-  add %rcx, %rax
 
+  add %rax, %rcx
 
   pop %rbp
   ret
