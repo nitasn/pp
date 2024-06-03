17e37eab3bcd317aed5ae231ae736a4da5c1e86e
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-06-03 18:13:15 +0300

    .

--- strings/b64.c
+++ strings/b64.c
@@ -1,7 +1,9 @@
 #include "libstr.h"
-
+#include "stdio.h"
 
 int b64_distance(char str1[MAX_STR], char str2[MAX_STR])
 {
+  printf("Input: '%s' \n", str1);
+  printf("Input: '%s' \n", str2);
 	return 77;
 }
