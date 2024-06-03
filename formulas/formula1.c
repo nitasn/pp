b41c0a6f1b4af65e677fd886ebce362fb40e477b
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-06-03 15:49:33 +0300

    .

--- formulas/formula1.c
+++ formulas/formula1.c
@@ -15,47 +15,14 @@ float formula1_sequential(float *x, unsigned int length)
   return sqrtf(1 + cbrtf(sum) / product);
 }
 
-float formula1_parallel_old(float *x, unsigned int length)
+float formula1(float *x, unsigned int length)
 {
   __m128 sum_sqrt_x = _mm_setzero_ps();
-  __m128 prod_x2_plus_1 = _mm_setzero_ps();
+  __m128 prod_x2_plus_1 = _mm_set1_ps(1.0f); // initialize to 1 for multiplication
 
   __m128 ones = _mm_set1_ps(1.0f);
 
-  for (size_t i = 0; i < length; ++i)
-  {
-    __m128 x_vec = _mm_load_ps(x + i); // load four floats
-
-    __m128 sqrt_x_vec = _mm_sqrt_ps(x_vec);
-    sum_sqrt_x = _mm_add_ps(sum_sqrt_x, sqrt_x_vec);
-
-    __m128 x2_vec = _mm_mul_ps(x_vec, x_vec);
-    __m128 x2_vec_plus_1 = _mm_add_ps(x2_vec, ones);
-    prod_x2_plus_1 = _mm_mul_ps(prod_x2_plus_1, x2_vec_plus_1);
-  }
-
-  sum_sqrt_x = _mm_hadd_ps(sum_sqrt_x, sum_sqrt_x);
-  sum_sqrt_x = _mm_hadd_ps(sum_sqrt_x, sum_sqrt_x);
-  float sum_sqrt_x_f = _mm_cvtss_f32(sum_sqrt_x);
-
-  // Manual reduction for product since hmul_ps doesn't exist
-  float prod_array[4];
-  _mm_storeu_ps(prod_array, prod_x2_plus_1);
-  float prod_x2_plus_1_f = prod_array[0] * prod_array[1] * prod_array[2] * prod_array[3];
-
-  float fraction = cbrtf(sum_sqrt_x_f) / prod_x2_plus_1_f;
-
-  return sqrtf(1 + fraction);
-}
-
-float formula1_parallel(float *x, unsigned int length)
-{
-  __m128 sum_sqrt_x = _mm_setzero_ps();
-  __m128 prod_x2_plus_1 = _mm_set1_ps(1.0f); // Initialize to 1 for multiplication
-
-  __m128 ones = _mm_set1_ps(1.0f);
-
-  // Adjust the loop to process four floats at a time
+  // adjust the loop to process four floats at a time
   for (size_t i = 0; i < length; i += 4)
   {
     __m128 x_vec = _mm_loadu_ps(x + i); // load four floats, use loadu for potentially unaligned data
@@ -68,23 +35,16 @@ float formula1_parallel(float *x, unsigned int length)
     prod_x2_plus_1 = _mm_mul_ps(prod_x2_plus_1, x2_vec_plus_1); // accumulating the product
   }
 
-  // Horizontal add to sum up all elements in sum_sqrt_x
+  // sum of the four numbers in sum_sqrt_x
   sum_sqrt_x = _mm_hadd_ps(sum_sqrt_x, sum_sqrt_x);
   sum_sqrt_x = _mm_hadd_ps(sum_sqrt_x, sum_sqrt_x);
   float sum_sqrt_x_f = _mm_cvtss_f32(sum_sqrt_x);
 
-  // Manual reduction for product since hmul_ps doesn't exist
+  // product of the four numbers in prod_x2_plus_1 (manually... because hmul_ps doesn't exist)
   float prod_array[4];
   _mm_storeu_ps(prod_array, prod_x2_plus_1);
   float prod_x2_plus_1_f = prod_array[0] * prod_array[1] * prod_array[2] * prod_array[3];
 
-  // Calculate the final expression
   float fraction = cbrtf(sum_sqrt_x_f) / prod_x2_plus_1_f;
-
   return sqrtf(1.0f + fraction);
-}
-
-float formula1(float *x, unsigned int length)
-{
-  return formula1_parallel(x, length);
 }
