1e0f79147b7582b7a789f52a1e27bbdfae30c137
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-06-03 17:37:54 +0300

    stuff

---
+++ strings/b64.c
@@ -0,0 +1,7 @@
+#include "libstr.h"
+
+
+int b64_distance(char str1[MAX_STR], char str2[MAX_STR])
+{
+	return 77;
+}
---
+++ strings/hamming.s
@@ -0,0 +1,11 @@
+.section .data
+
+
+.section .text
+
+.globl hamming_dist
+
+hamming_dist:
+
+	mov $17, %rax
+	ret
